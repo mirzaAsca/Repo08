21da101d573b6274bb2d146c84f97fb344d1b36e
mirzaAsca <90396304+mirzaAsca@users.noreply.github.com>
2021-09-21 00:05:26 +0200

    Update index.js

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -9,7 +9,7 @@ localScope();
 
  // Only change code above this line
  // myVariable is not defined outside of localScope
- if (typeof myVariable != "undefined"){
+ if (typeof myVariable != "undefined") {
     console.log('outside localScope', myVariable)
  } else {
     console.log('outside localScope UNDEFINED!!!')
