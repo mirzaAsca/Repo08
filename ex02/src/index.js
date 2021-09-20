1ea85accacf96a491d0df38ac0299b50983703da
mirzaAsca <90396304+mirzaAsca@users.noreply.github.com>
2021-09-21 00:01:35 +0200

    Update index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -6,4 +6,4 @@ function functionPrintInfo(name, age) {
   functionPrintInfo("John", 23);
   functionPrintInfo("Michael", 43);
   // Write code above this line
-  module.exports=functionPrintInfo;
+  module.exports = functionPrintInfo;
